920cf6681456d5b4846d95cba6e02efbfe555e64
Cris Motinha <cristinamotinham@gmail.com>
2017-04-18 21:02:19 -0300

    Creates new request based on json, parses it and return the same thing, now grabbing infos from all pages

--- app.py
+++ app.py
@@ -10,8 +10,12 @@ def index():
     me = {
     "name": "Cristina M.",
     }
-    podium = crawler.crawl("http://woobox.com/2evorj/gallery/HOrALzX1uVs", me)
-    return render_template('dinamic.html', me = me, ranking = crawler.get_podium(podium))
+    votes = []
+    votes += crawler.crawl("http://woobox.com/2evorj/context/votepage?page=1&marker=52&ajax=1", me, votes)
+    votes += crawler.crawl("http://woobox.com/2evorj/context/votepage?page=2&marker=52&ajax=1", me, votes)
+    votes += crawler.crawl("http://woobox.com/2evorj/context/votepage?page=3&marker=52&ajax=1", me, votes)
+    final_podium = crawler.get_podium(votes, me)
+    return render_template('dinamic.html', me = me, ranking = final_podium)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
--- src/crawler.py
+++ src/crawler.py
@@ -1,43 +1,36 @@
 import urllib.request
-from bs4  import BeautifulSoup
+from bs4 import BeautifulSoup
 import json
 import re
 
-def crawl(url, me):
+def crawl(url, me, votes_list):
     page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, "html.parser")
-
-    pattern = re.compile('var context = (.*\s\S.*);')
-    scripts = soup.find_all('script')
-    for script in scripts:
-        if(pattern.search(str(script.string))):
-            data = pattern.search(script.string)
-            content = json.loads(data.groups()[0])
-
+    # soup = BeautifulSoup(page, "html.parser")
+    # raw_content = soup.decode("utf-8")
+    content = json.loads(page.read().decode('utf-8'), strict=False)
     contestants = content['offer']['uploads']['data']
-    votes = []
+    votes_list = []
     for contestant in contestants:
         if contestant['uploaded_by'] == me["name"]:
             me["pic"] = contestant['url']
-        votes.append({"contestant":contestant['uploaded_by'], "votes":contestant["votes"]})
+        votes_list.append({"contestant":contestant['uploaded_by'], "votes":int(contestant["votes"])})
+    return votes_list
 
-    newlist = sorted(votes, key=lambda k: k['votes'], reverse=True) 
 
+def get_podium(newlist, me):
+    print(newlist)
+    newlist = sorted(newlist, key=lambda k: k['votes'], reverse=True)
     for i in newlist:
         if i['contestant'] == me["name"]:
             me["position"] = newlist.index(i) + 1
-
-    return newlist
-
-
-def get_podium(newlist):
+    print(newlist)
     return {'first':
-                {"name":newlist[0]["contestant"],
-                "votes": newlist[0]["votes"]},
-            'second':
-                {"name":newlist[1]["contestant"],
-                    "votes": newlist[1]["votes"]},
-            'third':
-                {"name":newlist[2]["contestant"],
-                    "votes": newlist[2]["votes"]},
-            }
+    {"name":newlist[0]["contestant"],
+    "votes": newlist[0]["votes"]},
+    'second':
+    {"name":newlist[1]["contestant"],
+    "votes": newlist[1]["votes"]},
+    'third':
+    {"name":newlist[2]["contestant"],
+    "votes": newlist[2]["votes"]},
+    }
